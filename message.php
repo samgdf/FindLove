ad193a59ed8d6123a217f5d2f81863cc95e41451
Nikolay Feldman <nf139@cs.rutgers.edu>
2014-10-19 05:57:00 -0400

    message post fix

--- message.php
+++ message.php
@@ -2,9 +2,9 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$number = $_GET['number'];
+$phone = $_GET['phone'];
 $name = $_GET['name'];
 
-system("curl -X POST -H \"Content-Type: application/json\" -d '{\"members\":[{\"nickname\":\"".$name."\", \"phone_number\":\"+1 ".$number."\"}]}' https://api.groupme.com/v3/groups/10669601/members/add?token=9802f6203999013215fe06ee6d143d9f");
+system("curl -X POST -H \"Content-Type: application/json\" -d '{\"members\":[{\"nickname\":\"".$name."\", \"phone_number\":\"+1 ".$phone."\"}]}' https://api.groupme.com/v3/groups/10669601/members/add?token=9802f6203999013215fe06ee6d143d9f");
 
 ?>
