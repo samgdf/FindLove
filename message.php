60c58f45a48dba44721b09553698c9d04071cf5a
Nikolay Feldman <nf139@cs.rutgers.edu>
2014-10-19 06:08:21 -0400

    changing groupme group

--- message.php
+++ message.php
@@ -5,6 +5,6 @@ ini_set('display_errors', 1);
 $phone = $_GET['phone'];
 $name = $_GET['name'];
 
-system("curl -X POST -H \"Content-Type: application/json\" -d '{\"members\":[{\"nickname\":\"".$name."\", \"phone_number\":\"+1 ".$phone."\"}]}' https://api.groupme.com/v3/groups/10669601/members/add?token=9802f6203999013215fe06ee6d143d9f");
+system("curl -X POST -H \"Content-Type: application/json\" -d '{\"members\":[{\"nickname\":\"".$name."\", \"phone_number\":\"+1 ".$phone."\"}]}' https://api.groupme.com/v3/groups/10669686/members/add?token=9802f6203999013215fe06ee6d143d9f");
 
 ?>
